748eeb0dd47adc26c0c57944ea30572bddfbb307
Tarek Meftah <sudtechnics@gmail.com>
2019-11-05 17:03:49 +0100

    this code prior to rendering on the client

---
+++ plugins/fireauth.js
@@ -0,0 +1,23 @@
+import {
+  auth
+} from '@/services/firebase.js'
+
+export default (context) => {
+  const {
+    store
+  } = context
+
+  return new Promise((resolve, reject) => {
+    auth.onAuthStateChanged((user) => {
+      const {
+        email,
+        uid
+      } = user
+      store.commit('SET_USER', {
+        email,
+        uid
+      })
+      resolve()
+    })
+  })
+}
